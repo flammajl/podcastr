001fca42026a94f601452cbbf3f859e9e6074b95
João Luiz <jleaopitta@gmail.com>
2021-04-21 15:16:24 -0300

    add: player component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,19 @@
 import { AppProps } from 'next/app';
+import { Header } from '../components/Header';
 import '../styles/global.scss';
+import styles from '../styles/app.module.scss';
+import { Player } from '../components/Player';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  );
 }
 
 export default MyApp;
