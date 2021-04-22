2e50f9ea6759c60661403d91963bf341cb9fc3c9
João Luiz <jleaopitta@gmail.com>
2021-04-21 21:44:31 -0300

    add: podcast episodes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,142 @@
-export default function Home(): JSX.Element {
-  return <h1>Index</h1>;
+import { GetStaticProps } from 'next';
+import Image from 'next/image';
+import { format, parseISO } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
+import { api } from '../services/api';
+import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+
+import styles from './home.module.scss';
+
+interface Episode {
+  id: string;
+  title: string;
+  members: string;
+  thumbnail: string;
+  file: {
+    duration: string;
+    url: string;
+  };
+  description: string;
+  publishedAt: string;
 }
+
+interface HomeProps {
+  latestEpisodes: Array<Episode>;
+  allEpisodes: Array<Episode>;
+}
+
+export default function Home({
+  latestEpisodes,
+  allEpisodes,
+}: HomeProps): JSX.Element {
+  return (
+    <div className={styles.homePage}>
+      <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+        <ul>
+          {latestEpisodes.map(episode => (
+            <li key={episode.id}>
+              <Image
+                src={episode.thumbnail}
+                alt={episode.title}
+                width={192}
+                height={192}
+                objectFit="cover"
+              />
+
+              <div className={styles.episodeDetails}>
+                <a href="#a">{episode.title}</a>
+                <p>{episode.members}</p>
+                <span>{episode.publishedAt}</span>
+                <span>{episode.file.duration}</span>
+              </div>
+
+              <button type="button">
+                <img src="/play-green.svg" alt="Tocar episódio" />
+              </button>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section className={styles.allEpisodes}>
+        <h2>Todos os episódios</h2>
+
+        <table cellSpacing={0}>
+          <thead>
+            <th />
+            <th>Podcast</th>
+            <th>Integrantes</th>
+            <th>Data</th>
+            <th>Duração</th>
+            <th />
+          </thead>
+          <tbody>
+            {allEpisodes.map(episode => (
+              <tr key={episode.id}>
+                <td style={{ width: 72 }}>
+                  <Image
+                    width={120}
+                    height={120}
+                    src={episode.thumbnail}
+                    alt={episode.title}
+                    objectFit="cover"
+                  />
+                </td>
+                <td>
+                  <a href="">{episode.title}</a>
+                </td>
+                <td>{episode.members}</td>
+                <td style={{ width: 100 }}>{episode.publishedAt}</td>
+                <td>{episode.file.duration}</td>
+                <td>
+                  <button type="button">
+                    <img src="/play-green.svg" alt="Tocar episódio" />
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </section>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { data } = await api.get('', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc',
+    },
+  });
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      members: episode.members,
+      thumbnail: episode.thumbnail,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {
+        locale: ptBR,
+      }),
+      description: episode.description,
+      file: {
+        url: episode.file.url,
+        duration: convertDurationToTimeString(Number(episode.file.duration)),
+      },
+    };
+  });
+
+  const latestEpisodes = episodes.slice(0, 2);
+  const allEpisodes = episodes.slice(2, episodes.length);
+
+  return {
+    props: {
+      latestEpisodes,
+      allEpisodes,
+    },
+    revalidate: 60 * 60 * 8, // 8 hrs
+  };
+};
